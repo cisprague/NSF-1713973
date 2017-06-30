48375ceb70da7e31c472d7e79816717d7461b41b
CISprague <christopher.iliffe.sprague@gmail.com>
2017-06-30 17:29:21 +0900

    Further developed multilayer perceptron

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include "ml.hpp"
-#include "mlp.cpp"
+//#include "mlp.cpp"
 
-//using namespace Eigen;
+using namespace Eigen;
 using namespace std;
 
 int main(void) {
-  // structure
+  // define structure of network
   vector<int> shape = {5, 10, 2};
-  // network
+  // instantiate network
   ml::mlp net(shape);
-  // print
-  net.print();
+  // initialise randomly weights and biases
+  //net.random_weights();
+  net.compute_output(ml::mlp::Layer::Random(5,1));
+
   return 0;
-}
+};
--- src/ml.hpp
+++ src/ml.hpp
@@ -5,6 +5,7 @@
 #ifndef ML_HPP
 #define ML_HPP
 #include <eigen3/Eigen/Dense>
+
 using namespace std;
 using namespace Eigen;
 
@@ -13,16 +14,70 @@ namespace ml {
 
   // multilayer perceptron
   class mlp {
-    private:
+
+    public:
+
+      // define some usefull data types
+      typedef Matrix<float, Dynamic, Dynamic> Weight;
+      typedef vector<Weight> Weights;
+      typedef Matrix<float, Dynamic, 1> Layer;
+      typedef vector<Layer> Biases;
+
       vector<int> shape; // structure
       int ni; // # inputs
       int no; // # ouputs
       int nl; // # layers
       int dim; // parametre dimensions
-      vector<MatrixXd> weights;
-    public:
-      mlp(vector<int> shape); // constructor
-      void print(void); // printer
+      Weights W;
+      Biases B;
+
+      // constructor
+      mlp(vector<int> shape) {
+        shape = shape;
+        ni = shape.front();
+        no = shape.back();
+        nl = shape.size();
+        for (int i=0; i<nl-1; i++) {
+          // we compile the parametre dimensions
+          dim += shape[i+1]*(shape[i] + 1);
+          // initialise randomly the weights
+          W.push_back(Weight::Random(shape[i+1],shape[i]));
+          // initialise randomly the biases
+          B.push_back(Layer::Random(shape[i+1], 1));
+        };
+      };
+
+      // compute activation
+      Layer sigmoid(Layer input) {
+        for (int i=0; i<input.size(); i++) {
+          input[i] = 1 + exp(-input[i]);
+          input[i] = 1/input[i];
+        };
+        //cout << input << endl;
+        return input;
+      };
+
+      // compute output
+      Layer compute_output(Layer input) {
+        // for each layer except last
+        for (int i=0; i<nl-1; i++) {
+          input = W[i]*input + B[i];
+          input = sigmoid(input);
+        };
+        cout << input << endl;
+        return input;
+      };
+
+      // printing details
+      void print(void) {
+        cout << "Multilayer perceptron" << endl;
+        cout << "Inputs: " << ni << endl;
+        cout << "Outputs: " << no << endl;
+        cout << "Layers: " << nl << endl;
+        cout << "Dimensions: " << dim << endl;
+        //cout << "Weights:\n " << weights[0](0,0) << endl;
+      };
+
   };
 
 };
